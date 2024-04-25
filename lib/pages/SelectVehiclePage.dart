a6fa9c51a6c5e316d3463d029a2e2a05fdda088c
himanshurajhr8 <himanshurajhr8@gmail.com>
2024-04-26 02:42:43 +0530

    updated select vehicle page

--- lib/pages/SelectVehiclePage.dart
+++ lib/pages/SelectVehiclePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:park_ease/pages/homepage/homepage.dart';
-
 
 class SelectVehiclePage extends StatefulWidget {
   @override
@@ -8,166 +8,203 @@ class SelectVehiclePage extends StatefulWidget {
 }
 
 class _SelectVehiclePageState extends State<SelectVehiclePage> {
-  // Sample list of vehicles with name and vehicle number
-  final List<Map<String, String>> vehicles = [
-    {'name': 'BMW X7', 'number': 'BR46H031'},
-    {'name': 'Truck', 'number': 'XYZ789'},
-    {'name': 'Motorcycle', 'number': 'DEF456'},
-    {'name': 'Bicycle', 'number': 'GHI789'},
-    {'name': 'Bus', 'number': 'JKL012'},
-  ];
-
   String? selectedVehicle;
-
-  void _showAddVehicleDialog() {
-    String vehicleName = '';
-    String vehicleNumber = '';
-
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Add New Vehicle'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                decoration: InputDecoration(labelText: 'Vehicle Name'),
-                onChanged: (value) {
-                  vehicleName = value;
-                },
-              ),
-              TextField(
-                decoration: InputDecoration(labelText: 'Vehicle Number'),
-                onChanged: (value) {
-                  vehicleNumber = value;
-                },
-              ),
-            ],
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('Cancel'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // Add your logic to add the new vehicle
-                setState(() {
-                  vehicles.add({'name': vehicleName, 'number': vehicleNumber});
-                });
-                Navigator.of(context).pop();
-              },
-              child: Text('Add'),
-            ),
-          ],
-        );
-      },
-    );
-  }
+  String user = "himanshurajhr8";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         title: Text('Choose a Vehicle'),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: vehicles.length + 1, // Add 1 for the "Add New Vehicle" button
-              itemBuilder: (context, index) {
-                if (index == vehicles.length) {
-                  // Render the "Add New Vehicle" button at the end
-                  return ListTile(
-                    title: ElevatedButton(
-                      onPressed: () {
-                        _showAddVehicleDialog();
-                      },
-                      child: Text('Add New Vehicle'),
-                    ),
-                  );
-                } else {
-                  final vehicle = vehicles[index];
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selectedVehicle = vehicle['number'];
-                      });
-                    },
-                    child: Container(
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Color(0xFFe3f2fd),
-                        borderRadius: BorderRadius.circular(8.0),
-                        border: Border.all(
-                          color: selectedVehicle == vehicle['number']
-                              ? Colors.red // Change border color to blue if selected
-                              : Colors.transparent, // Otherwise, transparent border
-                        ),
-                      ),
-                      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                      child: Center(
-                        child: Row(
-                          children: [
-                            SizedBox(width: 12,),
-                            Image.asset(
-                              'assets/images/car.png', // Car icon
-                              height: 48.0, // Adjust height as needed
-                              width: 48.0, // Adjust width as needed
-                            ),
-                            SizedBox(width: 20.0), // Add some space between image and text
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  vehicle['name']!, // Vehicle name
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                                SizedBox(height: 4.0),
-                                Text(
-                                  '${vehicle['number']}',
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Spacer(), // Added spacer to push the icon to the right
-                            selectedVehicle == vehicle['number']
-                                ? Icon(Icons.circle_rounded, color: Colors.red) // Use red filled circle if selected
-                                : Icon(Icons.circle_outlined, color: Colors.black,),
-                            SizedBox(width: 12),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }
-              },
-            ),
-          ),
-          SizedBox(height: 10), // Add some space between the buttons
-          ElevatedButton(
-            onPressed: () {
-              // Add your logic for the "Continue" button
-              Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage())); // Replace the current route with the homepage
+    ),
+    body: StreamBuilder
+      (
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .doc(user)
+          .collection('vehicles')
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (!snapshot.hasData) {
+          return Center(child: Text('No vehicles found.'));
+        }
+        // Now you have the list of vehicles for the user
+        final vehicles =
+        snapshot.data!.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
 
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue, // Set the background color to blue
+        return Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: vehicles.length + 1,
+                itemBuilder: (context, index) {
+                  if (index == vehicles.length) {
+                    return ListTile(
+                      title: ElevatedButton(
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              String newVehicleName = '';
+                              String newVehicleNumber = '';
+
+                              return AlertDialog(
+                                title: Text('Add New Vehicle'),
+                                content: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    TextField(
+                                      onChanged: (value) {
+                                        newVehicleName = value;
+                                      },
+                                      decoration: InputDecoration(
+                                        labelText: 'Vehicle Name',
+                                      ),
+                                    ),
+                                    TextField(
+                                      onChanged: (value) {
+                                        newVehicleNumber = value;
+                                      },
+                                      decoration: InputDecoration(
+                                        labelText: 'Vehicle Number',
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                actions: [
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      // Validate input fields
+                                      if (newVehicleName.isNotEmpty &&
+                                          newVehicleNumber.isNotEmpty) {
+                                        // Add the new vehicle to Firestore
+                                        FirebaseFirestore.instance
+                                            .collection('users')
+                                            .doc(user)
+                                            .collection('vehicles')
+                                            .add({
+                                          'vehicleName': newVehicleName,
+                                          'vehicleId': newVehicleNumber,
+                                        }).then((value) {
+                                          // Close the dialog
+                                          Navigator.pop(context);
+                                        }).catchError((error) {
+                                          // Handle errors if any
+                                          print("Failed to add vehicle: $error");
+                                        });
+                                      }
+                                    },
+                                    child: Text('Add'),
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      // Close the dialog
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Cancel'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: Text('Add New Vehicle'),
+                      ),
+                    );
+                  }
+                  else {
+                    final vehicle = vehicles[index];
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedVehicle = vehicle['vehicleId'];
+                        });
+                      },
+                      child: Container(
+                        height: 100,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFe3f2fd),
+                          borderRadius: BorderRadius.circular(8.0),
+                          border: Border.all(
+                            color: selectedVehicle ==
+                                vehicle['vehicleId']
+                                ? Colors.red
+                                : Colors.transparent,
+                          ),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                        child: Center(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: 12,
+                              ),
+                              Image.asset(
+                                'assets/images/car.png',
+                                height: 48.0,
+                                width: 48.0,
+                              ),
+                              SizedBox(width: 20.0),
+                              Column(
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    vehicle['vehicleName']!,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16.0,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4.0),
+                                  Text(
+                                    '${vehicle['vehicleId']}',
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Spacer(),
+                              selectedVehicle ==
+                                  vehicle['vehicleId']
+                                  ? Icon(Icons.circle_rounded,
+                                  color: Colors.red)
+                                  : Icon(Icons.circle_outlined,
+                                  color: Colors.black),
+                              SizedBox(width: 12),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  }
+                },
+              ),
             ),
-            child: Text('Continue'),
-          ),
-        ],
-      ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => HomePage()));
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+              ),
+              child: Text('Continue'),
+            ),
+          ],
+        );
+      },
+    ),
     );
   }
 }
